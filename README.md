414faca7c66f1f8cd22ee385e3b624384dc10607
ReidHung <redgrandfa@gmail.com>
2025-07-28 00:34:35 +0800

    Implementation and README

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# websocket-aspnetcore-sample
+# Implement WebSockets in ASP.NET Core 8
+
+## Client JS
+```js
+// const ws = new WebSocket("ws://localhost:/ws"); 
+const ws = new WebSocket("wss://localhost:7024/ws"); // wss is secure connection
+ws.onopen = () => { ... }
+ws.onmessage = (e) => { ... } // e.data := received from server
+ws.send( ... )  
+ws.onclose = () => { ... }
+
+```
+
+## Server 
+### Server Receive:
+```csharp
+if (context.WebSockets.IsWebSocketRequest){
+    using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+    byte[] buffer = new byte[1024 * 4];
+    var ReceiveAsync = async () => await webSocket.ReceiveAsync(
+        new ArraySegment<byte>(buffer),
+        CancellationToken.None
+    );
+
+    WebSocketReceiveResult receiveResult = await ReceiveAsync();
+    while (!receiveResult.CloseStatus.HasValue)
+    {
+        //receiveResult.Count                  // The length in bytes of the data received this time.
+        //receiveResult.EndOfMessage           // bool: is received competely?
+        //receiveResult.MessageType            // enum: Text | Binary | Close
+        //receiveResult.CloseStatus            // enum: If not null, it indicates that the client has requested to close the connection.
+        //receiveResult.CloseStatusDescription // string
+
+        string receivedText = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+        Console.WriteLine($"Received: {receivedText}");
+
+        receiveResult = await ReceiveAsync();
+    }
+
+
+    // Close connection
+    await webSocket.CloseAsync(
+        receiveResult.CloseStatus.Value,
+        receiveResult.CloseStatusDescription,
+        CancellationToken.None);
+}
+
+```
+
+### Server Send:
+```csharp
+WebSocket ws = ...;
+ws.SendAsync( new ArraySegment<byte>(buffer),
+    WebSocketMessageType.Text,
+    endOfMessage: true,
+    CancellationToken.None);
+```
+
+## About CORS
+- The WebSocket protocol is upgraded from HTTP.
+- After the upgrade, CORS does not apply to the WebSocket connection.
+
+
+---
+## Usage
+1. Run Server Project
+2. Open `client.html` in a browser.
+3. Server sends messages periodically (by timer) and displays them on the web page.
+4. Client sends a message manually and sees it on the server console.
+
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,88 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+
+var app = builder.Build();
+app.UseWebSockets(); // required.
+
+var clients = new ConcurrentDictionary<Guid, WebSocket>();  // Keep track of all connected clients.
+
+
+
+// Server Receive:
+app.Use(async (context, next) =>  
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            // Upgrade TCP connection from an HTTP connection to the WebSocket protocol.
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+            // Register connected client
+            var id = Guid.NewGuid();
+            clients.TryAdd(id, webSocket); 
+
+
+            byte[] buffer = new byte[1024 * 4];
+            var ReceiveAsync = async () => await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer),
+                CancellationToken.None
+            );
+
+            WebSocketReceiveResult receiveResult = await ReceiveAsync();
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                //receiveResult.Count                  // The length in bytes of the data received this time.
+                //receiveResult.EndOfMessage           // bool: is received competely?
+                //receiveResult.MessageType            // enum: Text | Binary | Close
+                //receiveResult.CloseStatus            // enum: If not null, it indicates that the client has requested to close the connection.
+                //receiveResult.CloseStatusDescription // string
+
+                string receivedText = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+                Console.WriteLine($"Received: {receivedText}");
+
+                receiveResult = await ReceiveAsync();
+            }
+
+            // Unregister client
+            clients.TryRemove(new KeyValuePair<Guid, WebSocket>(id, webSocket));
+            // Close connection
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+        }
+    }
+    else
+    {
+        await next();
+    }
+});
+
+// Server Send:
+var timer = new Timer(async _ =>
+{
+    var msg = $"Server broadcast at {DateTime.Now:HH:mm:ss}";
+    var buffer = Encoding.UTF8.GetBytes(msg);
+    var tasks = clients.Values
+        .Where(ws => ws.State != WebSocketState.Closed)
+        .Select(ws =>
+            ws.SendAsync(
+                new ArraySegment<byte>(buffer),
+                WebSocketMessageType.Text,
+                endOfMessage: true,
+                cancellationToken: CancellationToken.None)
+        );
+    await Task.WhenAll(tasks);
+}, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
 
 app.Run();
